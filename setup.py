43f02562696608858d5f94b9d7d78c8beef23178
Aidan Clyens <aidanc@rogers.com>
2018-11-07 17:15:02 -0500

    Updated build

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ executables = [cx_Freeze.Executable("breakout.py")]
 
 cx_Freeze.setup(
     name = "Breakout",
-    options = {"build_exe":{"packages":["pygame"], "include_files":["arcade-background.jpg"]}},
+    options = {"build_exe":{"packages":["pygame"], "include_files":["fonts/AtariClassic-Regular.ttf"]}},
     description = "Breakout Game",
     executables = executables
 )
